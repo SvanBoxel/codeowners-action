cf107ce0c6b562e57da4aa80f33541b8d0f9f98f
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2022-08-29 17:23:31 +0200

    Fixes CODEOWNERS parse issue with blank lines and comments

--- src/extract-codeowners.ts
+++ src/extract-codeowners.ts
@@ -1,18 +1,21 @@
 import {CodeOwnersInfo} from '../types/codeowners'
 import {getFileContents} from './utils'
 
-const codeowners_re = new RegExp('^[^#](.*)$', 'mg')
-
 async function extractCodeOwners(path: string): Promise<CodeOwnersInfo> {
   const contents = await getFileContents(path)
-  const lines: RegExpMatchArray | null = contents.match(codeowners_re)
-  if (!lines) return {}
-  return lines.reduce((acc: CodeOwnersInfo, cur: string) => {
-    const [key, value] = cur.split(/(?<=^\S+)\s/)
-    if (!key || !value) return acc
-    acc[key] = value.split(/ /).filter(Boolean)
-    return acc
-  }, {})
+  if (!contents) return {}
+  return contents
+    .split(/\s*$\s*/m)
+    .filter(line => !line.startsWith('#'))
+    .reduce((acc: CodeOwnersInfo, cur: string) => {
+      const values = cur.split(/\s+/)
+      const key = values.shift()
+
+      if (key) {
+        acc[key] = values
+      }
+      return acc
+    }, {})
 }
 
 export default extractCodeOwners
